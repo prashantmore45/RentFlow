de83fa6da1b7193c1f5889c2ef406df2ac13480d
prashantmore45 <prashantmorepm05@gmail.com>
2026-01-21 02:21:32 +0530

    feat: Implement room management features in Dashboard and backend, including room deletion and fetching user-specific rooms

--- server/routes/roomRoutes.js
+++ server/routes/roomRoutes.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { createRoom, getRooms, getRoomById } from '../controllers/roomController.js';
+import { createRoom, getRooms, getRoomById, getMyRooms, deleteRoom } from '../controllers/roomController.js';
 
 const router = express.Router();
 
 router.get('/', getRooms);     
 router.post('/', createRoom); 
 router.get('/:id', getRoomById);
+
+router.get('/my-rooms/:user_id', getMyRooms);
+router.delete('/:id', deleteRoom);
 
 export default router;
